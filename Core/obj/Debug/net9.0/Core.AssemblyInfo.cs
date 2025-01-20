8cc03b6e26d06adc13ae148d1cf0cd1613823713
ElianG <eliangb0408@gmail.com>
2025-01-20 17:30:19 -0400

    Arreglado el error de paginado. [El error estaba en MedicosGetAllSpecification y devolvia una pagina fuera de los parametros permitidos.]

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee08b38ff8c78a80dc6d3a1b87588aeb96c028e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05751fc85805df366612742aa6de941d0649bb67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee08b38ff8c78a80dc6d3a1b87588aeb96c028e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05751fc85805df366612742aa6de941d0649bb67")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
